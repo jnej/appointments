e042b75cb7cfb41d4f43d81fdced105d43f62fb8
Amanda Johnson <amandajnej@gmail.com>
2017-06-09 10:57:11 +0100

    make corrections to fields not showing.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function()  {
   $("#formOne").submit(function(event) {
-    var nameInput = $("input#nameInput").val();
-    var descripInput =$("input#descrip").val();
-    var dateInput =$("input#date").val();
-    var timeInput =$("input#time").val();
+    var name = $("input#name").val();
+    var descrip =$("input#descrip").val();
+    var date =$("input#date").val();
+    var time =$("input#time").val();
 
-    $(".name").text(nameInput);
-    $(".descrip").text(descripInput);
-    $(".date").text(dateInput);
-    $(".time").text(timeInput);
+    $(".name").text(name);
+    $(".descrip").text(descrip);
+    $(".date").text(date);
+    $(".time").text(time);
 
     $("#confirm").show();
 
